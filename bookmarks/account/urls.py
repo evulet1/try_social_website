21587c22a00ea04d0ab2223227781b2add4ec3fd
evulet <elvirochkata@gmail.com>
2021-11-24 17:13:39 +0300

    reset pass

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # change password urls
+    path('', include('django.contrib.auth.urls')),
 ]
-
